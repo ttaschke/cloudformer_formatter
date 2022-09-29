12c9190929bbd544dc1fc0f2426deddd37dbbc8d
Tim Taschke <taschke@fout.jp>
2022-09-29 23:00:34 +0900

    Fix formatting and linting errors

--- tests/test_cloudformer_formatter.py
+++ tests/test_cloudformer_formatter.py
@@ -4,68 +4,43 @@ import cloudformer_formatter
 from unittest import mock
 
 CLOUDFORMER_OUTPUT = {
-  "AWSTemplateFormatVersion": "2010-09-09",
-  "Resources": {
-    "vpcb9bcc3cb": {
-      "Type": "AWS::EC2::VPC",
-      "Properties": {
-      }
+    "AWSTemplateFormatVersion": "2010-09-09",
+    "Resources": {
+        "vpcb9bcc3cb": {"Type": "AWS::EC2::VPC", "Properties": {}},
+        "vpcb545fcb": {"Type": "AWS::EC2::VPC", "Properties": {}},
+        "subnet502ab36f": {
+            "Type": "AWS::EC2::Subnet",
+            "Properties": {"VpcId": {"Ref": "vpcb9bcc3cb"}},
+        },
     },
-    "vpcb545fcb": {
-      "Type": "AWS::EC2::VPC",
-      "Properties": {
-      }
-    },
-    "subnet502ab36f": {
-      "Type": "AWS::EC2::Subnet",
-      "Properties": {
-        "VpcId": {
-          "Ref": "vpcb9bcc3cb"
-        }
-      }
-    }
-  }
 }
 
 EXPECTED_RESULT = {
-  "AWSTemplateFormatVersion": "2010-09-09",
-  "Resources": {
-    "VPC": {
-      "Type": "AWS::EC2::VPC",
-      "Properties": {
-      }
+    "AWSTemplateFormatVersion": "2010-09-09",
+    "Resources": {
+        "VPC": {"Type": "AWS::EC2::VPC", "Properties": {}},
+        "VPC2": {"Type": "AWS::EC2::VPC", "Properties": {}},
+        "Subnet": {"Type": "AWS::EC2::Subnet", "Properties": {"VpcId": {"Ref": "VPC"}}},
     },
-    "VPC2": {
-      "Type": "AWS::EC2::VPC",
-      "Properties": {
-      }
-    },
-    "Subnet": {
-      "Type": "AWS::EC2::Subnet",
-      "Properties": {
-        "VpcId": {
-          "Ref": "VPC"
-        }
-      }
-    }
-  }
 }
 
 
 class TestCloudformerFormatter(unittest.TestCase):
-
-    @mock.patch('cloudformer_formatter.write_output')
+    @mock.patch("cloudformer_formatter.write_output")
     def test_main(self, write_output):
 
         p1 = mock.patch("builtins.open", mock.MagicMock())
 
-        m = mock.MagicMock(side_effect = [CLOUDFORMER_OUTPUT])
+        m = mock.MagicMock(side_effect=[CLOUDFORMER_OUTPUT])
         p2 = mock.patch("json.load", m)
 
-        with p1 as p_open:
-            with p2 as p_json_load:
-                cloudformer_formatter.main(['cloudformer_output','output_location.json'])
-                write_output.assert_called_with(EXPECTED_RESULT,'output_location.json')
+        with p1 as p_open:  # noqa
+            with p2 as p_json_load:  # noqa
+                cloudformer_formatter.main(
+                    ["cloudformer_output", "output_location.json"]
+                )
+                write_output.assert_called_with(EXPECTED_RESULT, "output_location.json")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
